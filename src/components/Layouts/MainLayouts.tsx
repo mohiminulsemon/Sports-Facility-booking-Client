6d032f673a926bbe02447697dea21acdec32845c
mohiminulsemon <mohiminulsemon80@gmail.com>
2024-09-28 23:33:49 +0600

    redux api.ts updated

--- src/components/Layouts/MainLayouts.tsx
+++ src/components/Layouts/MainLayouts.tsx
@@ -1,13 +1,13 @@
-import Footer from "@/pages/shared/Footer";
+// import Footer from "@/pages/shared/Footer";
 import Navbar from "@/pages/shared/Navbar";
 import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
-    <div className="">
+    <div className="bg-[#000924] ">
       <Navbar />
       <Outlet ></Outlet>
-      <Footer></Footer>
+      {/* <Footer></Footer> */}
     </div>
   );
 };
